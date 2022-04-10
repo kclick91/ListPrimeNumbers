daec47c60aaab0c15c113f80bd39afeb51155389
Kelly Click <kclick91@gmail.com>
2022-04-10 18:51:55 -0400

    Add files via upload

---
+++ PrimeNumberLists/src/Main.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+
+    public static ArrayList<ArrayList<Integer>> lists = new ArrayList<ArrayList<Integer>>();
+
+
+    public static void main(String[] args)
+    {
+        int count = 3;
+        lists.add(new ArrayList<>());
+        lists.get(0).add(2);
+        //list primes up to stopping point
+        int stoppingPoint = 50;
+        int numberOfLists = 1;
+        while (count < stoppingPoint)
+        {
+            boolean isPrime = true;
+            for (int j = 0; j < numberOfLists; j++)
+            {
+                if ((count % lists.get(j).get(0)) == 0)
+                {
+                    isPrime = false;
+                    lists.get(j).add(count);
+                    break;
+                }
+                else
+                {
+
+                }
+            }
+            if(isPrime == true)
+            {
+                numberOfLists = numberOfLists + 1;
+                lists.add(new ArrayList<>());
+                lists.get(numberOfLists - 1).add(count);
+                System.out.println(count + " is a prime.");
+            }
+            count = count + 1;
+        }
+
+    }
+}
